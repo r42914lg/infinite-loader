2d57ed801c331f4fd4604574740d210f384de999
lgdars22 <lgdars22@gmail.com>
2023-08-04 09:43:51 +0300

    multiple versions fix

--- app/src/main/java/com/r42914lg/myrealm/migrations/DBMigrationHelper.kt
+++ app/src/main/java/com/r42914lg/myrealm/migrations/DBMigrationHelper.kt
@@ -9,9 +9,21 @@ import io.realm.RealmSchema
 class DBMigrationHelper : RealmMigration {
 
     override fun migrate(realm: DynamicRealm, oldVersion: Long, newVersion: Long) {
-        migration1to2(realm.schema)
-        migration2to3(realm.schema)
-        migration3to4(realm.schema)
+        val schema = realm.schema
+        var verInProgress = oldVersion
+
+        if (verInProgress == 0L) {
+            migration1to2(schema)
+            verInProgress++
+        }
+
+        if (verInProgress == 1L) {
+            migration2to3(schema)
+            verInProgress++
+        }
+
+        if (verInProgress < newVersion)
+            throw IllegalStateException("Migration missing from $oldVersion to $newVersion")
     }
 
     private fun migration1to2(schema: RealmSchema) {
@@ -19,19 +31,13 @@ class DBMigrationHelper : RealmMigration {
         userSchema?.run {
             addField("prop3New", String::class.java, FieldAttribute.REQUIRED)
             renameField("prop1", "prop1Renamed")
-        }
-    }
-
-    private fun migration2to3(schema: RealmSchema) {
-        val userSchema = schema.get(ItemEntityRealm::class.java.simpleName)
-        userSchema?.run {
             transform {
                 it.set("prop3New", "M " + it.get<String>("prop1Renamed") + it.get<String>("prop2"))
             }
         }
     }
 
-    private fun migration3to4(schema: RealmSchema) {
+    private fun migration2to3(schema: RealmSchema) {
         val userSchema = schema.get(ItemEntityRealm::class.java.simpleName)
         userSchema?.run {
             removeField("prop2")
